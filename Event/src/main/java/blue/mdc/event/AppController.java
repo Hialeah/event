4753ce05f45f53d70bc6f5087653827e09d2ea2d
Abraham <abraham@19cah.com>
2018-06-19 10:43:24 -0400

    open new windows on a click

--- Event/src/main/java/blue/mdc/event/AppController.java
+++ Event/src/main/java/blue/mdc/event/AppController.java
@@ -6,6 +6,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+//////////
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class AppController implements Initializable {
     
@@ -16,6 +23,23 @@ public class AppController implements Initializable {
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello Worlk!");
+        
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("/fxml/add/AddEvent.fxml"));   
+            /* 
+            * if "fx:controller" is not set in fxml
+            * fxmlLoader.setController(NewWindowController);
+            */
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage = new Stage();
+            stage.setTitle("New Window");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            Logger logger = Logger.getLogger(getClass().getName());
+            logger.log(Level.SEVERE, "Failed to create new Window.", e);
+        }
     }
     
     @Override
